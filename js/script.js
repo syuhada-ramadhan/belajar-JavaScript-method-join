d3c5c2efb599cbfabfa7d1c058c9016483ed18d6
syuhada-ramadhan <suhadahada18@gmail.com>
2021-02-16 16:48:56 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// array hewan
+const hewan = ["Kambing", "Kelinci", "Kerbau", "Kucing", "Sapi"];
+
+console.log(hewan.join());
+console.log(hewan.join(""));
+console.log(hewan.join("-"));
+console.log(hewan.join(", "));
+console.log(hewan.join(" + "));
